fb6c7b51878fdc491a9091785c0f5e64da779e9f
fahsa <fahsa@umich.edu>
2018-02-11 18:18:04 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [AngularFireDatabase]
+})
+
+export class AppComponent {
+  title = 'foley database';
+  //description = 'Angular4-Firebase Demo';
+
+  itemValue = '';
+  items: FirebaseListObservable<any[]>;
+
+  constructor(db: AngularFireDatabase) {
+    this.items = db.list('/items');
+  }
+
+  onSubmit() {
+    this.items.push({content: this.itemValue});
+    this.itemValue = '';
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAMCAQsXThiN7vSYgeFE7rGQ7Faicu8bVM",
+    authDomain: "foley-database.firebaseapp.com",
+    databaseURL: "https://foley-database.firebaseio.com",
+    projectId: "foley-database",
+    storageBucket: "foley-database.appspot.com",
+    messagingSenderId: "848798280948"
+  }
+};
